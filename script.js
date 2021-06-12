b8b5a63ef4416f23edd5783a2cc9c87f26394d58
duckymke <evitarussellmagill@gmail.com>
2021-06-12 19:13:21 -0400

    project

---
+++ script.js
@@ -0,0 +1,72 @@
+const reviews = [
+    {
+        id: 1,
+        author: 'mei',
+        title: 'best daughter ever',
+        // img: '',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere quidem vero sed cum ullam reiciendis temporibus odio illum veritatis maxime. Exercitationem eveniet molestias quae, iusto velit tenetur fugiat veritatis laudantium?',
+    },
+    {
+        id: 2,
+        author: 'hiro',
+        title: 'best son ever',
+        // img: '',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere quidem vero sed cum ullam reiciendis temporibus odio illum veritatis maxime. Exercitationem eveniet molestias quae, iusto velit tenetur fugiat veritatis laudantium?',
+    },
+    {
+        id: 3,
+        author: 'sakura',
+        title: 'tied for best daughter ever',
+        // img: '',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere quidem vero sed cum ullam reiciendis temporibus odio illum veritatis maxime. Exercitationem eveniet molestias quae, iusto velit tenetur fugiat veritatis laudantium?',
+    }
+];
+
+// const img = document.getElementById('person-img')
+const author = document.getElementById('author');
+const title = document.getElementById('title');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// starting item
+let currentItem = 0;
+
+// show person based on item
+function showPerson(person) {
+    const item = reviews[person];
+    author.textContent = item.author;
+    title.textContent = item.title;
+    info.textContent = item.text;
+}
+
+
+// initial item
+window.addEventListener('DOMContentLoaded', function() {
+    showPerson(currentItem);
+});
+
+// show next person item
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+// show previous person
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }  
+    showPerson(currentItem);
+});
+
+// random person
+randomBtn.addEventListener('click', function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+});
